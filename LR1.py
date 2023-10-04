5de73c9570eecf24287c2701cf0bf7e0e677bd61
Кристина <kristi090217@gmail.com>
2023-10-04 23:37:17 +0300

    жду мердж реквест!!!!!!пофиксила баги :D

--- LR1.py
+++ LR1.py
@@ -9,33 +9,7 @@ import numdifftools as nd
 
 # Определение функций, которые мы можем оптимизировать
 def target_function(x, y):
-    result = ((x ** 2 + y - 11) ** 2) + ((x + y ** 2 - 7) ** 2)
-    return np.round(result, 6)
-
-# Определение функции для обновления графика функции
-def update_function_plot():
-    ax.cla()
-    x_range = np.linspace(x_interval_min.get(), x_interval_max.get(), 100)
-    y_range = np.linspace(y_interval_min.get(), y_interval_max.get(), 100)
-    X, Y = np.meshgrid(x_range, y_range)
-    Z = target_function(X, Y)
-    ax.plot_surface(X, Y, Z, cmap='viridis')
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    ax.set_zlabel('Z')
-    ax.set_xticks(np.arange(x_interval_min.get(), x_interval_max.get() + 1, x_axis_interval.get()))
-    ax.set_yticks(np.arange(y_interval_min.get(), y_interval_max.get() + 1, y_axis_interval.get()))
-    canvas.draw()
-    run_optimization()
-
-# Функция для обновления поля "Выполнение и результаты"
-def update_results(text_widget, results):
-    text_widget.config(state=tk.NORMAL)
-    text_widget.delete(1.0, tk.END)
-    for result in results:
-        text_widget.insert(tk.END,
-                           f"Шаг {result[2]}: Координаты ({result[0]:.2f}, {result[1]:.2f}), Значение функции: {result[3]:.4f}\n")
-    text_widget.config(state=tk.DISABLED)
+    return ((x ** 2 + y - 11) ** 2) + ((x + y ** 2 - 7) ** 2)
 
 # Функция для градиента
 def gradient(function, input):
@@ -62,17 +36,24 @@ def run_optimization():
     delay = delay_var.get()
 
     ax.cla()
-    ax.plot_surface(X, Y, Z, cmap='viridis')
+    x_range = np.linspace(x_interval_min.get(), x_interval_max.get(), 100)
+    y_range = np.linspace(y_interval_min.get(), y_interval_max.get(), 100)
+    X, Y = np.meshgrid(x_range, y_range)
+    Z = target_function(X, Y)
+    ax.plot_surface(X, Y, Z, cmap='viridis',alpha=0.7)
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
+    ax.set_xticks(np.arange(x_interval_min.get(), x_interval_max.get() + 1, x_axis_interval.get()))
+    ax.set_yticks(np.arange(y_interval_min.get(), y_interval_max.get() + 1, y_axis_interval.get()))
 
     function_choice = function_var.get()
     if function_choice == "Функция Химмельблау":
         target_func = target_function
 
     results = []
-
+    results_text.config(state=tk.NORMAL)
+    results_text.delete(1.0, tk.END)
     for k in range(max_iterations):
         (gx, gy) = gradient(target_func, [x0, y0])
 
@@ -95,14 +76,21 @@ def run_optimization():
         else:
             x0, y0 = x1, y1
 
-        results.append((x0, y0, k, round(f1,5)))
-        ax.scatter([x0], [y0], [round(f1,5)], color='red')
+        results.append((x0, y0, k,f1))
+        ax.scatter([x0], [y0], [f1], color='red',s=10)
+        results_text.insert(tk.END,
+                           f"Шаг {k}: Координаты ({x0:.2f}, {y0:.2f}), Значение функции: {f1:.7f}\n")
 
         canvas.draw()
         root.update()
         time.sleep(delay)
 
-    update_results(results_text, results)
+    length=len(results)-1
+    # ax.scatter(results[length][0],results[length][1],results[length][3],color='black')
+    print(results)
+    print(length)
+    ax.scatter(results[length][0], results[length][1], results[length][3], color='black',marker='x',s=60)
+    results_text.config(state=tk.DISABLED)
 
 # Создание окна приложения
 root = tk.Tk()
@@ -136,9 +124,7 @@ step_entry.grid(row=3, column=1)
 iterations_entry.grid(row=4, column=1)
 delay_entry.grid(row=5, column=1)
 
-# Создание панели для отображения результатов
-# results_frame = ttk.Frame(root)
-# results_frame.pack(side=tk.BOTTOM, padx=10, pady=10)
+
 ttk.Label(param_frame, text="Выполнение и результаты").grid(row=20, column=1)
 results_text = tk.Text(param_frame, wrap=tk.WORD, height=10, width=40, state=tk.DISABLED)
 results_text.grid(row=21, column=1)
@@ -156,9 +142,6 @@ canvas = FigureCanvasTkAgg(fig, master=root)
 canvas_widget = canvas.get_tk_widget()
 canvas_widget.pack(side=tk.RIGHT, padx=20, pady=10)
 
-# Создание панели для параметров функции и ее отображения
-# settings_frame = ttk.Frame(root)
-# settings_frame.pack(side=tk.TOP, padx=10, pady=10)
 
 ttk.Label(param_frame, text="Функция и отображение ее графика").grid(row=9, column=0)
 ttk.Label(param_frame, text="Выберите функцию").grid(row=10, column=0)
@@ -196,7 +179,7 @@ x_axis_interval_entry.grid(row=16, column=1)
 y_axis_interval_entry.grid(row=17, column=1)
 
 # Создание кнопки "Применить"
-apply_settings_button = ttk.Button(param_frame, text="Выполнить", command=update_function_plot)
+apply_settings_button = ttk.Button(param_frame, text="Выполнить", command=run_optimization)
 apply_settings_button.grid(row=7, column=0, columnspan=2)
 
 root.mainloop()
